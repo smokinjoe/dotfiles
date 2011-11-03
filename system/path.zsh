ac11e78e70fd2bf8817ae85ecdb3336f6df4dd5e
Joe Ekiert <jekiert@seventhartgroup.com>
2011-11-03 17:41:46 -0400

    added new path to my ruby gems cellar

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:/usr/local/Cellar/gems/1.8/bin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
